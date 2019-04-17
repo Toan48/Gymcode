2cc4439731a6cbca9aad514ec2dd6463c6944e4f
Toan48 <dinhtuongtoan48@gmail.com>
2019-04-17 14:47:29 +0700

    update operator

---
+++ js/operator.js
@@ -0,0 +1,9 @@
+
+let inputWidth = prompt("enter the width: ");
+let inputHeight = prompt("enter the height: ");
+
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+
+let area = width * height;
+document.write("the area is: " +area);
