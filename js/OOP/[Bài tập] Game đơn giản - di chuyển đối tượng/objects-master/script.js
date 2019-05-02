eee0fe8a5b9b1cd95f305ca03b1ffa0ae53f379f
Toan48 <dinhtuongtoan48@gmail.com>
2019-05-02 14:20:38 +0700

    updated

--- js/OOP/[Bài tập] Game đơn giản - di chuyển đối tượng/objects-master/script.js
+++ js/OOP/[Bài tập] Game đơn giản - di chuyển đối tượng/objects-master/script.js
@@ -2,11 +2,12 @@
  * Created by nhatnk on 4/26/17.
  */
 
-function Hero(image, top, left, size){
+function Hero(image, top, left, size, speed){
   this.image = image;
   this.top = top;
   this.left = left;
   this.size = size;
+  this.speed = speed;
 
   this.getHeroElement = function(){
     return '<img width="'+ this.size + '"' +
@@ -16,48 +17,49 @@ function Hero(image, top, left, size){
   }
 
   this.moveRight = function(){
-    this.left += 20;
+    this.left += this.speed;
     console.log('ok: ' + this.left);
   }
 
   this.moveLeft = function()
   {
-    this.left =- 20;
+    this.left -= this.speed;
     console.log('ok: ' + this.left);
   }
 
  this.moveDown = function()
  {
-   this.top +=20;
+   this.top +=this.speed;
    console.log('ok: ', this.top);
  }
 
  this.moveTop = function()
  {
-   this.top -=20;
+   this.top -= this.speed;
    console.log('ok: ', this.top);
  }
 
 }
 
-var hero = new Hero('pikachu.png', 20, 30, 200);
+var hero = new Hero('pikachu.png', 0, 0, 200, 100);
 
 function start(){
-  if(hero.left < window.innerWidth - hero.size){
+  if(hero.left < window.innerWidth - hero.size && hero.top == 0){
     hero.moveRight();
   }
-  else if(hero.left >= window.innerWidth - 250 && hero.top < window.innerHeight - 280)
+  else if(hero.left >= window.innerWidth - hero.size && hero.top < window.innerHeight - hero.size)
   {
     hero.moveDown();
   }
-  else if(hero.top >= window.innerHeight - hero.size && hero.left >= 2)
+  else if(hero.top >= window.innerHeight - hero.size && hero.left >= 20)
   {
     hero.moveLeft();
   }
   else
   {
-    //hero.moveTop();
+    hero.moveTop();
   }
+  
   document.getElementById('game').innerHTML = hero.getHeroElement();
   setTimeout(start, 500)
 }
--- js/practice variable operator/example/javascrpt.js
+++
@@ -1,4 +0,0 @@
-function sayHi()
-{
-    alert('say hi');
-}
