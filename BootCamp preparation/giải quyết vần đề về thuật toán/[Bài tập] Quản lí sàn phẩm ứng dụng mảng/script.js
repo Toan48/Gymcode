8147b7c8b024e1ce182e8d203aba97333ae14eb8
Toan48 <dinhtuongtoan48@gmail.com>
2019-05-17 13:47:22 +0700

    update

--- BootCamp preparation/giải quyết vần đề về thuật toán/[Bài tập] Quản lí sàn phẩm ứng dụng mảng/script.js
+++ BootCamp preparation/giải quyết vần đề về thuật toán/[Bài tập] Quản lí sàn phẩm ứng dụng mảng/script.js
@@ -4,31 +4,24 @@ var app = new function()
     this.el = document.getElementById('products');
     this.products = ['SUZUKI', 'YAMAHA', 'FORD', 'TOYOTA', 'KIA'];
 
-    this.count = function(data)
-    {
+    this.count = function(data){
         var el = document.getElementById('counter');
         var name = 'product';
-        if(data)
-        {
-            if(data > 1)
-            {
+        if(data){
+            if(data > 1){
                 name = 'product';
             }
             el.innerHTML = data + ' ' + name;
         }
-        else
-        {
+        else{
             el.innerHTML = 'No' + name;
         }
     };
     
-    this.fetchAll = function()
-    {
+    this.fetchAll = function(){
         var data = '';
-        if(this.products.length > 0)
-        {
-            for(i = 0; i < this.products.length; i++)
-            {
+        if(this.products.length > 0){
+            for(i = 0; i < this.products.length; i++){
                 data += '<tr>';
                 data += '<td>' + this.products[i] + '</td>';
                 data += '<td><button onclick="app.edit('+ i +')">Edit</button></td>';
@@ -40,29 +33,24 @@ var app = new function()
         return this.el.innerHTML = data;
     };
 
-    this.add = function()
-    {
+    this.add = function(){
         el = document.getElementById('add-name');
         var product = el.value;
-        if(product)
-        {
+        if(product){
             this.products.push(product.trim());
             el.value = '';
             this.fetchAll();
         }
     };
 
-    this.edit = function(item)
-    {
+    this.edit = function(item){
         var el = document.getElementById('edit-name');
         el.value = this.products[item];
         document.getElementById('spoiler').style.display = 'block';
         self = this;
-        document.getElementById('saveEdit').onsubmit = function()
-        {
+        document.getElementById('saveEdit').onsubmit = function(){
             var product = el.value;
-            if(product)
-            {
+            if(product){
                 self.products.splice(item, 1 , product.trim());
                 self.fetchAll();
                 closeInput();
@@ -71,8 +59,7 @@ var app = new function()
 
     };
 
-    this.delete = function(item)
-    {
+    this.delete = function(item){
         this.products.splice(item, 1);
         this.fetchAll();
     }   
