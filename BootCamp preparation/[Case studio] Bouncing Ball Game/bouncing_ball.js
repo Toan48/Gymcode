b2f1fbea5c7c7841ee34ea47ef68ef02db449714
Toan48 <dinhtuongtoan48@gmail.com>
2019-06-02 19:40:30 +0700

    updated

--- BootCamp preparation/[Case studio] Bouncing Ball Game/bouncing_ball.js
+++ BootCamp preparation/[Case studio] Bouncing Ball Game/bouncing_ball.js
@@ -1,15 +1,19 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-var ballRadius = 10;
 var x = canvas.width/2;
-var y = canvas.height-30;
+var y = canvas.height - 50;
 var dx = 2;
 var dy = -2;
+var ballRadius = 10; //radius of the ball
 var paddleHeight = 10;
 var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
+var paddleX = (canvas.width - paddleWidth)/2;
 var rightPressed = false;
 var leftPressed = false;
+var score = 0;
+
+console.log(x);
+console.log(paddleX);
 
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
@@ -39,6 +43,7 @@ function drawBall() {
     ctx.fill();
     ctx.closePath();
 }
+
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
@@ -47,19 +52,28 @@ function drawPaddle() {
     ctx.closePath();
 }
 
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: "+score, 8, 20);
+
+}
+
 function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height); // clear the specified pixels within in  a given rectangle
     drawBall();
     drawPaddle();
-    
+    drawScore();
+    //change the ditection of the movemet when the ball is touching the left or right edge of canvans 
     if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
         dx = -dx;
     }
+    //change the ditection of the movemet when the ball is touching the top or bottom edge of canvans 
     if(y + dy < ballRadius) {
         dy = -dy;
     }
-    else if(y + dy > canvas.height-ballRadius) {
-        if(x > paddleX && x < paddleX + paddleWidth) {
+    else if(y + dy > canvas.height - ballRadius) {
+        if( paddleX < x &&  paddleX + paddleWidth > x) {
             dy = -dy;
         }
         else {
@@ -68,7 +82,7 @@ function draw() {
             clearInterval(interval); // Needed for Chrome to end game
         }
     }
-    
+
     if(rightPressed && paddleX < canvas.width-paddleWidth) {
         paddleX += 7;
     }
@@ -76,8 +90,8 @@ function draw() {
         paddleX -= 7;
     }
     
-    x += dx;
-    y += dy;
+    x += dx; // increase width of the ball
+    y += dy; // decrease height of the ball
 }
 
 var interval = setInterval(draw, 10);
